e3b18805b359cde9d62db80e1d9bc23ae42e0fed
xhydromovie <xhydromovie@gmail.com>
2019-02-09 23:33:03 +0100

    Working parse script

--- scraping/scrap-igrit.py
+++ scraping/scrap-igrit.py
@@ -4,5 +4,35 @@ from bs4 import BeautifulSoup
 url = 'https://www.igrit.pl/kategoria/gielda-ziemniakow-42?category=gielda-ziemniakow-42&page=1'
 
 page = requests.get(url)
+soup = BeautifulSoup(page.text, 'html.parser')
 
-print(page.text[:240])
+items = soup.find_all(class_="article-list")
+
+parsed_items = {}
+
+# print(date, item_type, number, " ".join(price)
+for item in items:
+    print("petla")
+    date = item.find(class_="created-at").string
+    item_type = item.find(class_="type").string
+    price = item.find(class_="price").string.split()
+    localization = item.find(class_="addressInfo").find("p").contents[2].split(",")
+    name = localization[0].split()
+    voivodeship = localization[1].split()
+    city = localization[2].split()
+    print(name, voivodeship, city)
+    try:
+        number = item.find(class_="phoneNumber").attrs["data-phone"]
+    except KeyError:
+        number = "Brak numeru"
+
+    parsed_item = {
+        "date": date,
+        "item_type": item_type,
+        "phone_number": number,
+        "price": price,
+        }
+    
+    print(parsed_item)
+    parsed_items[date] = parsed_item
+
