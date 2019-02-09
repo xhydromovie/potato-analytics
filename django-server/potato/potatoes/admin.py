4b3cbdcf1d9413ba2245d06e20bd09c34743a205
xhydromovie <xhydromovie@gmail.com>
2019-02-10 00:46:29 +0100

    Upgrade

--- django-server/potato/potatoes/admin.py
+++ django-server/potato/potatoes/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import IgritItem
 # Register your models here.
+
+admin.site.register(IgritItem)
--- django-server/potato/potatoes/models.py
+++ django-server/potato/potatoes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class IgritItem(models.Model):
+    id = models.AutoField(primary_key=True)
+    date = models.DateField()
+    price = models.IntegerField()
+
+    def __str__(self):
+        return str(self.id)
