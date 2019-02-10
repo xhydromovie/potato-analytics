10d6a6d5b3f46f9e967bc9d74d541fc0fc4de54a
xhydromovie <xhydromovie@gmail.com>
2019-02-10 18:36:31 +0100

    Move scripts

--- django-server/potato/potatoes/models.py
+++ django-server/potato/potatoes/models.py
@@ -4,8 +4,14 @@ from datetime import datetime
 # Create your models here.
 class IgritItem(models.Model):
     id = models.AutoField(primary_key=True)
-    date = models.DateField()
+    date = models.DateTimeField()
     price = models.IntegerField()
+    is_price = models.BooleanField()
+    path = models.CharField(max_length=100, default="") # e.g "/ogloszenie/ziemniaki-fioletowe-901593"
+    voivodeship = models.CharField(max_length=100, default="")
+    city = models.CharField(max_length=100, default="")
+    item_type = models.CharField(max_length=20, default="")
+    phone_number = models.CharField(max_length=20, default="")
 
     def __str__(self):
         return str(self.id)
